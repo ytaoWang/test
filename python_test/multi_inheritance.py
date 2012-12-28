1232a6a7e86779d0868000463e4aec98918f00b2
yt0907.Wang@gmail.com <yt0907.Wang@gmail.com>
2012-12-28 20:26:46 +0800

    multi_inheritance

---
+++ python_test/multi_inheritance.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+
+class C7(object):
+    def test(self):
+        print 'test in C7'
+
+class C4(C7):
+    def test(self):
+        print 'test in C4'
+
+class C5(C7):
+    def test(self):
+        print 'test in C5'
+
+class C6(C7):
+    def test(self):
+        print 'test in C6'
+
+class C2(C4,C5):
+    def test(self):
+        print 'test in C2'
+
+class C3(C4,C6):
+    pass
+
+class C1(C2,C3):
+    pass
+
+class C0(C2,C3):
+    def test(self):
+        print 'test in C0'
+        super(C3,self).test()
+
+if __name__ == "__main__":
+    
+    c1 = C1()
+    c1.test()
+    c0 = C0()
+    c0.test()
+    print C7.__mro__
+    print C6.__mro__
+    print C5.__mro__
+    print C4.__mro__
+    print C3.__mro__
+    print C2.__mro__
+    print C1.__mro__
+    print C0.__mro__
